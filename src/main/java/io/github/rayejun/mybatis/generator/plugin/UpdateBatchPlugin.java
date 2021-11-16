c940dfd7b68bf540eb10bd5fc3c49bfb1c8bdf93
rayejun <1196296741@qq.com>
2021-11-16 20:33:48 +0800

    文件

---
+++ src/main/java/io/github/rayejun/mybatis/generator/plugin/UpdateBatchPlugin.java
@@ -0,0 +1,38 @@
+package io.github.rayejun.mybatis.generator.plugin;
+
+import io.github.rayejun.mybatis.generator.codegen.javamapper.*;
+import io.github.rayejun.mybatis.generator.codegen.xmlmapper.*;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.xml.Document;
+import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
+import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
+
+import java.util.List;
+
+public class UpdateBatchPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+        AbstractXmlElementGenerator elementGenerator = new UpdateBatchElementGenerator();
+        elementGenerator.setContext(context);
+        elementGenerator.setIntrospectedTable(introspectedTable);
+        elementGenerator.addElements(document.getRootElement());
+        return super.sqlMapDocumentGenerated(document, introspectedTable);
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, IntrospectedTable introspectedTable) {
+        AbstractJavaMapperMethodGenerator methodGenerator = new UpdateBatchMethodGenerator();
+        methodGenerator.setContext(context);
+        methodGenerator.setIntrospectedTable(introspectedTable);
+        methodGenerator.addInterfaceElements(interfaze);
+        return super.clientGenerated(interfaze, introspectedTable);
+    }
+}
